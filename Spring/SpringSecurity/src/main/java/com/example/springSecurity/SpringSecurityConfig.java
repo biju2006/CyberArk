473b3d4f0851088677a7ff16063564513648b124
Rony Keren <47735932+ronykeren@users.noreply.github.com>
2019-04-30 14:58:21 +0300

    Add files via upload

--- Spring/SpringSecurity/src/main/java/com/example/springSecurity/SpringSecurityConfig.java
+++ Spring/SpringSecurity/src/main/java/com/example/springSecurity/SpringSecurityConfig.java
@@ -1,44 +1,31 @@
 package com.example.springSecurity;
 
-import java.util.Properties;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity  //enables roles & URL pattern mapping
+@EnableGlobalMethodSecurity(securedEnabled = true)  //enables @Secured method mapping
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private AuthenticationEntryPoint authEntryPoint;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-				.anyRequest().authenticated()
-				.and().httpBasic()
-				.authenticationEntryPoint(authEntryPoint);
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	  auth.inMemoryAuthentication().withUser("user").password("{noop}123456").roles("USER");
+	  auth.inMemoryAuthentication().withUser("admin").password("{noop}123456").roles("ADMIN");
+	  auth.inMemoryAuthentication().withUser("mng").password("{noop}123456").roles("MNG");
 	}
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		single user:
-//		auth.inMemoryAuthentication().withUser("david").password("1234").roles("USER");
-		auth.userDetailsService(inMemoryUserDetailsManager());
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	  http.authorizeRequests()
+		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+		.antMatchers("/mng/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MNG')")
+		.and().formLogin();	
 	}
-	
-	@Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        final Properties users = new Properties();
-        users.put("david","1234,USER"); //username, password,role 
-        users.put("eve","5678,USER");
-        return new InMemoryUserDetailsManager(users);
-    }
 
 }
